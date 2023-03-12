9f5e0c91820b1f6f0359dea78b3c205923aa0803
Hedgehog-warts <aldebaran0902@gmail.com>
2023-03-12 14:53:57 -0400

    m1 work on from here

--- app/src/main/java/algonquin/cst2335/androidapplicationproject/NickMain.java
+++ app/src/main/java/algonquin/cst2335/androidapplicationproject/NickMain.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class NickMain extends AppCompatActivity {
-
+    // m1
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
