2454bddf294729853aaa482d6a548dda91dfa24b
zeng0043 <zeng0043@algonquinlive.com>
2023-03-20 17:54:12 -0400

    Finished Requirement 4: Created 1 Toast, Snackbar, and a AlertDialog

--- app/src/main/java/algonquin/cst2335/androidapplicationproject/NewYorkTimes.java
+++ app/src/main/java/algonquin/cst2335/androidapplicationproject/NewYorkTimes.java
@@ -1,11 +1,18 @@
 package algonquin.cst2335.androidapplicationproject;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import algonquin.cst2335.androidapplicationproject.databinding.ActivityNewYorkTimesBinding;
 
@@ -18,6 +25,9 @@ public class NewYorkTimes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        /*
+        Requirement 1: Each person’s project must have a RecyclerView somewhere to present items in a list.
+        */
         binding = ActivityNewYorkTimesBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_new_york_times);
 
@@ -38,5 +48,16 @@ public class NewYorkTimes extends AppCompatActivity {
                 return 0;
             }
         });
+
+        /*
+        Requirement 9 part 1/3: Each activity must have at least 1 Toast.
+        */
+        Context context = getApplicationContext();
+        CharSequence text = "Finished creating the NewYorkTimes Activity.";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
+
 }
+
