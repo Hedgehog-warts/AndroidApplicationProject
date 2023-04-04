c5475ce8ab97f199502c8ff76fdcc4c178a0770d
zeng0043 <zeng0043@algonquinlive.com>
2023-04-04 14:00:03 -0400

    can open a browser when clicking a URL

--- app/src/main/java/algonquin/cst2335/androidapplicationproject/XingyunFragment.java
+++ app/src/main/java/algonquin/cst2335/androidapplicationproject/XingyunFragment.java
@@ -1,5 +1,7 @@
 package algonquin.cst2335.androidapplicationproject;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -26,10 +28,18 @@ public class XingyunFragment extends Fragment {
         binding.headlineText.setText(selected.headline);
         binding.urlText.setText(selected.url);
         binding.dateText.setText(selected.date);
-//        binding.messageText.setText(selected.message);
-//        binding.timeText.setText(selected.timeSent);
-//        binding.databaseText.setText("Id = " + selected.id);
-//        binding.sendOrReceiveText.setText("Is Sent: " + selected.isSentButton);
+
+        // This code creates an intent with the ACTION_VIEW action and a data URI containing the URL.
+        // This intent will launch the user's default web browser to display the URL when the user clicks on the urlText TextView.
+        binding.urlText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = selected.url;
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
 
         return binding.getRoot();
     }
