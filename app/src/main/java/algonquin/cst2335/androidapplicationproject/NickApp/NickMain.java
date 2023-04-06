168802c931b2a497fb93010a883a97a353e504fc
Nick_Kostik <kost0033@algonquin.live.com>
2023-04-06 19:15:40 -0400

    working on search

--- app/src/main/java/algonquin/cst2335/androidapplicationproject/NickApp/NickMain.java
+++ app/src/main/java/algonquin/cst2335/androidapplicationproject/NickApp/NickMain.java
@@ -5,14 +5,37 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+//import org.json.JSONException;
+//import com.android.volley.Request;
+//import com.android.volley.RequestQueue;
+//import com.android.volley.Response;
+//import com.android.volley.toolbox.ImageRequest;
+//import com.android.volley.toolbox.JsonObjectRequest;
+//import com.android.volley.toolbox.Volley;
+//import com.bumptech.glide.Glide;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
 
 import algonquin.cst2335.androidapplicationproject.DoyoungApp.DoyoungMain;
 import algonquin.cst2335.androidapplicationproject.R;
@@ -23,9 +46,9 @@ import algonquin.cst2335.androidapplicationproject.databinding.ActivityNickMainB
 // final
 public class NickMain extends AppCompatActivity {
 
-
+    private List<ImageData> imageDataList;
     ActivityNickMainBinding binding;
-    private ArrayList<String> pics = new ArrayList<>();
+//    protected RequestQueue queue = null;
 
     @Override
     public void setSupportActionBar(@Nullable Toolbar toolbar) {
@@ -80,15 +103,66 @@ public class NickMain extends AppCompatActivity {
         return true;
     }
 
+    private void validateInputs() {
+        EditText widthEditText = findViewById(R.id.widthEditText);
+        EditText heightEditText = findViewById(R.id.heightEditText);
+
+        String widthStr = widthEditText.getText().toString().trim();
+        String heightStr = heightEditText.getText().toString().trim();
+
+
+        if (widthStr.isEmpty() && heightStr.isEmpty()) {
+            Toast.makeText(this, "Please enter width and height values.", Toast.LENGTH_SHORT).show();
+        } else if (widthStr.isEmpty()) {
+            Toast.makeText(this, "Please enter width value.", Toast.LENGTH_SHORT).show();
+        } else if (heightStr.isEmpty()) {
+            Toast.makeText(this, "Please enter height value.", Toast.LENGTH_SHORT).show();
+        } else {
+            // Both inputs are valid, you can perform your desired action here
+        }
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nick_main);
 
+
         Toolbar myToolbar = findViewById(R.id.myToolbar);
         setSupportActionBar(myToolbar);
 
         binding = ActivityNickMainBinding.inflate(getLayoutInflater());
+
+
+        binding.searchButton.setOnClickListener(clk -> {
+            EditText widthEditText = findViewById(R.id.widthEditText);
+            EditText heightEditText = findViewById(R.id.heightEditText);
+            validateInputs();
+            String url = "https://placekitten.com/" + heightEditText.getText().toString() + "/" + widthEditText.getText().toString();
+
+            RequestQueue queue = Volley.newRequestQueue(this);
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                    response -> {
+                        try {
+                            // Get the image URL from the response object
+                            String imageUrl = "https://placekitten.com/" + widthEditText.getText().toString() + "/" + heightEditText.getText().toString();
+
+                            // Load the image into the ImageView using Glide library
+                            Glide.with(NickMain.this).load(imageUrl).into(binding.catImageView);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            Toast.makeText(NickMain.this, "Error fetching image.", Toast.LENGTH_SHORT).show();
+                        }
+                    },
+                    error -> {
+                        Toast.makeText(NickMain.this, "Error fetching image.", Toast.LENGTH_SHORT).show();
+                    }
+            );
+            // Add the request to the RequestQueue
+            queue.add(request);
+        });
+
 
     }
 }
